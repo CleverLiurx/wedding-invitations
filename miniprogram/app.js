e94c849b9bf1dce29af85ef58871002dd75ad360
roxin <13131451002@163.com>
2024-03-28 14:40:32 +0800

    feat: 图片框组件

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -11,31 +11,31 @@ App({
       });
     }
 
-    const audio = wx.createInnerAudioContext({
-      useWebAudioImplement: false
-    })
-    audio.onPlay(() => {
-      Event.emit('music:status', true)
-    })
-    audio.onStop(() => {
-      Event.emit('music:status', false)
-    })
-    audio.onPause(() => {
-      Event.emit('music:status', false)
-    })
-    audio.src = 'cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/kwlink_d_out.mp3'
-    audio.autoplay = true
-    audio.loop = true
-    Event.on('music:status', play => {
-      if (play) {
-        audio.play()
-      } else {
-        audio.pause()
-      }
-    })
-    Event.on('music:getStatus', () => {
-      Event.emit('music:status', !audio.paused)
-    })
+    // const audio = wx.createInnerAudioContext({
+    //   useWebAudioImplement: false
+    // })
+    // audio.onPlay(() => {
+    //   Event.emit('music:status', true)
+    // })
+    // audio.onStop(() => {
+    //   Event.emit('music:status', false)
+    // })
+    // audio.onPause(() => {
+    //   Event.emit('music:status', false)
+    // })
+    // audio.src = 'cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/kwlink_d_out.mp3'
+    // audio.autoplay = true
+    // audio.loop = true
+    // Event.on('music:status', play => {
+    //   if (play) {
+    //     audio.play()
+    //   } else {
+    //     audio.pause()
+    //   }
+    // })
+    // Event.on('music:getStatus', () => {
+    //   Event.emit('music:status', !audio.paused)
+    // })
 
     this.globalData = {};
   }
