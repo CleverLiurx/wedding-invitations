380f443cc928ee54f71836d36306e2f0a8ae218c
roxin <13131451002@163.com>
2024-03-31 22:46:12 +0800

    feat: 修改文案

--- miniprogram/utils/album.js
+++ miniprogram/utils/album.js
@@ -1,39 +1,43 @@
 const album = [
   {
     cnName: "婚纱照",
-    cover: "cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/image/cover/wedding_dress.png",
+    cover: "/images/album/wedding_dress.png",
     name: "wedding_dress",
     description: '刘 容 新',
-    descriptionImage: '/images/wo.png',
+    descriptionImage: '/images/album/wo.png',
     pos: 'left',
+    locked: false,
   },
   {
     cnName: "婚礼当日",
-    cover: "cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/image/cover/wedding_day.png",
+    cover: "/images/album/wedding_day.png",
     name: "wedding_day",
-    description: '石 蕊',
-    descriptionImage: '/images/men.png',
+    description: '石　蕊',
+    descriptionImage: '/images/album/men.png',
     pos: 'right',
+    locked: false,
   },
   {
     cnName: "碎碎念",
-    cover: "cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/image/cover/trivia.png",
+    cover: "/images/album/trivia.png",
     name: "trivia",
     description: [
       '一 屋 / 二 人',
       '三 餐 / 四 季'
     ],
     pos: 'left',
+    locked: true,
   },
   {
     cnName: "私密",
-    cover: "cloud://cloud1-7gmcl9gy8f6410db.636c-cloud1-7gmcl9gy8f6410db-1321709151/image/cover/private.png",
+    cover: "/images/album/private.png",
     name: "private",
     description: [
       '不 远 千 里',
       '只 为 遇 见 你'
     ],
     pos: 'right',
+    locked: true,
   },
 ]
 
